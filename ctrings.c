c09a5dd05eb3f41e1cb9630ae57964788f5d4a15
nikitaBor <nborisov@stuy.edu>
2017-10-05 11:19:12 -0400

    added comments and imporved mystrcpy function

--- ctrings.c
+++ ctrings.c
@@ -2,16 +2,21 @@
 #include <string.h>
 #include "ctrings.h"
 
+//mystrlen takes in a char pointer s and returns and int for length by counting
+//  how many characters there are before NULL is reached
 int mystrlen(char *s){
   int i = 0;
   while(*s++){i++;}
   return i;
 }
 
+//mystrcpy takes in two char pointers: the source from which chars are copied
+//  and the dest into which chars are copied. It copies chars up to and including
+//  the NULL in source
 char * mystrcpy( char *dest, char *source ){
-  char *source2 = source;
-  while(*dest++ = *source++){}
-  return dest-mystrlen(source2)-1;
+  char *dest2 = dest; //need a copy since dest will be changed
+  while(*dest++ = *source++){} //sets chars in dest to value of chars in source
+  return dest2; //returns to the position dest originaly pointed to
 }
 
 char * mystrncat( char *dest, char *source, int n){
